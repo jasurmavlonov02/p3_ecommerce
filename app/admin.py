598b6f85cbe55001960c1a462486e9b6ab33b385
jasurmavlonov02 <jasurmavlonov24@gmail.com>
2022-09-16 15:03:07 +0500

    commit message

---
+++ app/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from app.models import Product, Category, User
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'price', 'amount']
+    # fields = ['title', 'price', 'description']
+
+
+#
+# admin.site.register(Product)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name', 'id', 'slug', 'product_count']
+    fields = ['image', 'name']
+
+    def product_count(self, obj):
+        return obj.product_set.count()
+
+admin.site.register(User, UserAdmin)
